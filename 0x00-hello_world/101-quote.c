a9b8a611f28c29d2d4e647a74dbe2c5c3101301e
ayman-gassi <itsmeaymane2003@gmail.com>
2023-06-15 09:46:15 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,4 +12,3 @@ int main(void)
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,4 +9,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
